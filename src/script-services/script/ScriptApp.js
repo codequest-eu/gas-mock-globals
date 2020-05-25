027aa3bfd274c95b428372aa4ae7c0faf3173bfb
Wiktor Obrębski <psychowico@gmail.com>
2020-05-25 10:46:31 +0200

    Add missing SciprtApp function

--- src/script-services/script/ScriptApp.js
+++ src/script-services/script/ScriptApp.js
@@ -2,6 +2,10 @@ class ScriptApp {
   static getScriptId () {
     return 'dummy-script-id';
   }
+
+  static getIdentityToken() {
+      return 'dummy-identity-token';
+  }
 }
 
 module.exports = ScriptApp;
